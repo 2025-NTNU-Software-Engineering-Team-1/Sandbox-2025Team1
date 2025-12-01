cc62ee2d9da4e032029292ebdb69d6dbffff003c
Camel <kg.cheng777@gmail.com>
2025-12-01 23:08:17 +0800

    test(sandbox): add custom-checker float sample problem and coverage

---
+++ problem/custom-checker-float-test/src/solution.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    
+    // Output each number on a separate line with high precision
+    for (int i = 0; i < n; i++) {
+        double num;
+        cin >> num;
+        cout << fixed << setprecision(10) << num << endl;
+    }
+    
+    return 0;
+}
