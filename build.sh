8aa71fe6f6bac867aba3799e1a7bf36294b6761f
max52015 <choyihun52015@gmail.com>
2025-12-28 08:19:48 +0800

    還原初始

--- build.sh
+++ build.sh
@@ -3,13 +3,13 @@
 # sandbox binary
 
 
-# rm -f sandbox
-# wget https://github.com/2025-NTNU-Software-Engineering-Team-1/C-Sandbox-2025Team1/releases/latest/download/sandbox
-# chmod +x sandbox
-# # sandbox_interactive binary
-# rm -f sandbox_interactive
-# wget https://github.com/2025-NTNU-Software-Engineering-Team-1/C-Sandbox-2025Team1/releases/latest/download/sandbox_interactive
-# chmod +x sandbox_interactive
+rm -f sandbox
+wget https://github.com/2025-NTNU-Software-Engineering-Team-1/C-Sandbox-2025Team1/releases/latest/download/sandbox
+chmod +x sandbox
+# sandbox_interactive binary
+rm -f sandbox_interactive
+wget https://github.com/2025-NTNU-Software-Engineering-Team-1/C-Sandbox-2025Team1/releases/latest/download/sandbox_interactive
+chmod +x sandbox_interactive
 
 # c_cpp / python3 / interactive / custom_checker_scorer / network router / system router (AI)
 docker build -t noj-c-cpp -f c_cpp_dockerfile . --no-cache
